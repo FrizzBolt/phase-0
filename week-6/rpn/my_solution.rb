12b6a51d30f1a6557ee9f88374d19528273b976c
FrizzBolt <joshua.a.abrams@gmail.com>
2015-10-19 12:00:38 -0500

    Add RPN Bonus Challenge

---
+++ week-6/rpn/my_solution.rb
@@ -0,0 +1,44 @@
+@@ -0,0 +1,43 @@
+#Initial Solution
+
+class RPNCalculator
+  
+  def initialize
+    @stack = Array.new
+  end
+
+  def operator(x)
+  result = 0
+  case x
+  when "*"
+      result = @stack[-2] * @stack[-1]
+  when "+"
+      result = @stack[-2] + @stack[-1]
+  when "-"
+      result = @stack[-2] - @stack[-1]
+  end
+  2.times { @stack.pop }
+  @stack << result
+  end
+  
+  def evaluate(equation)
+    @equation = equation.split(" ")
+    @equation.each do |x|
+      if x.to_i != 0
+        @stack << x.to_i
+      elsif x == "*" || x == "+" || x == "-"
+        operator(x)
+      elsif 0
+        p 0
+      else
+        p "Error"
+      end
+    end
+
+    p @stack[0]
+  
+  end
+    
+end
+
+# I didn't have time to refactor, but here's my initial solution. It passes all of the rspec tests.
